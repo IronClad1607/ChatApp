19d8659c0dee02b97c955f1e4236bd88aedfc658
Ishaan Jain <ishaan160799@gmail.com>
2020-06-17 21:01:38 +0530

    login: firebase congifured

--- app/src/main/java/com/ironclad/chatapp/MainActivity.kt
+++ app/src/main/java/com/ironclad/chatapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.ironclad.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+        }
     }
 }
